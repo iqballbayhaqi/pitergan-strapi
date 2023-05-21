c941850d94dddfd3e03824105e3cbf273f1297f5
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2023-05-22 01:35:42 +0700

    update models

---
+++ src/api/primary-project/routes/primary-project.ts
@@ -0,0 +1,7 @@
+/**
+ * primary-project router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::primary-project.primary-project');
---
+++ src/api/primary-project/services/primary-project.ts
@@ -0,0 +1,7 @@
+/**
+ * primary-project service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::primary-project.primary-project');
